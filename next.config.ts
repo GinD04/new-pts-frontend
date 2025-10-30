b555e6ce1afdc1b7a02fe44ce1a17a7d951d069c
Antipov Nikolay <12minram@gmail.com>
2025-10-30 14:39:15 +0300

    Setup CI/CD
    Setup CI with:
    - build
    - sonar
    - deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
-    output: 'standalone',
+    output: 'export',
+    distDir: 'dist',
+    trailingSlash: true,
+    skipTrailingSlashRedirect: false,
+    basePath: process.env.PUBLIC_URL || '',
 };
 
 export default nextConfig;
