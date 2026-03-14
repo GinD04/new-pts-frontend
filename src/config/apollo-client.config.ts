87d13eea64d2c18789e7e94c2ae62d54ea29d17f
Darya Tsukanova <darya@ocwn.spb.ru>
2026-03-14 22:49:56 +0300

    fix apollo and axios configs, add logout

--- src/config/apollo-client.config.ts
+++ src/config/apollo-client.config.ts
@@ -2,6 +2,9 @@ import { BASE_URL } from '@/shared';
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    link: new HttpLink({ uri: `${BASE_URL}/graphql` }),
+    link: new HttpLink({
+        uri: `${BASE_URL}/graphql`,
+        credentials: 'include',
+    }),
     cache: new InMemoryCache(),
 });
